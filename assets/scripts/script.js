901fa2002dd63bd1ffa00414b931fa06543ca738
Douglas Fujii <doug.fujii@gmail.com>
2021-04-20 09:48:09 -0300

    add image for each work

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,19 +1,26 @@
 const WORKS = 'works';
 const works = [
-    'jogo da memória',
-    'jogo da velha',
+    'jogo_da_memoria',
+    'jogo_da_velha',
     'churrascometro',
-    'pagina de captura',
-    'formulário',
+    'pagina_de_captura',
+    'formulario',
     'calculadora'
 ];
 const workGrid = document.getElementById('work_grid');
 
 // cria um elemento para cada trabalho em works
-works.forEach(() => {return createWorkElement(WORKS, showDetails)});
+works.forEach((element) => {return createWorkElement(element, WORKS, showDetails)});
 
-function createWorkElement(c, f) {
+function createWorkElement(element, c, f) {
     const div = document.createElement('div');
+    const img = document.createElement('img');
+
+    img.src = './assets/images/' + element + '.png';
+
+    img.classList.add('img');
+
+    div.appendChild(img);
 
     div.classList.add(c);
    
