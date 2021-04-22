8db61b53c069b7536409e6dc0c38a9f0d5281bfb
Douglas Fujii <doug.fujii@gmail.com>
2021-04-22 13:54:28 -0300

    finish the works section and add github links

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -3,7 +3,7 @@ const works = [
     'jogo_da_memoria',
     'jogo_da_velha',
     'churrascometro',
-    'pagina_de_captura',
+    'site-momo-lamen',
     'formulario',
     'calculadora'
 ];
@@ -32,7 +32,7 @@ function createWorkElement(element, c, f) {
     workElement.id = element;
 
     workElement.classList.add(c);
-   
+
     workElement.addEventListener('click', f);
 
     workGrid.appendChild(workElement);
@@ -42,60 +42,145 @@ function showDetails() {
     const h1 = document.createElement('h1');
     const p1 = document.createElement('p');
     const p2 = document.createElement('p');
+    const gitHub = document.createElement('a');
     const button = document.createElement('button');
-    
-    switch(this.id) {
+
+    switch (this.id) {
         case 'jogo_da_memoria':
+            // título
             h1.innerHTML = 'Jogo da Memória';
+
+            // descrição
             p1.innerHTML = 'Esse jogo faz parte do curso de desenvolvimento web que estou fazendo. Após finalizado, foi adicionado uma tela de início, onde as cartas começam viradas para cima e quando começa o jogo, são viradas para baixo e embaralhadas. Além disso, foi adicionado um contador de tempo e outro de movimentos. Dependendo da quantidade de movimentos usados para terminar o jogo, a tela final mostra textos diferentes.';
 
+            // texto para github
+            p2.innerHTML = 'Confira o código no ';
+
+            // link para github
+            gitHub.href = 'https://github.com/DouglasFujii/jogo-da-memoria'
+            gitHub.innerHTML = 'GitHub';
+            gitHub.target = '_blank'
+
+            // adiciona os elementos html
             modal.appendChild(h1);
             modal.appendChild(p1);
+            p2.appendChild(gitHub);
+            modal.appendChild(p2);
 
             break;
 
         case 'jogo_da_velha':
+            // titulo
             h1.innerHTML = 'Jogo da Velha';
-            p.innerHTML = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec consectetur leo mollis ipsum consequat, in fringilla sem sodales. Phasellus ultricies nec augue suscipit tincidunt. Nunc sagittis nulla at efficitur volutpat. Vestibulum turpis massa, vehicula quis faucibus non, condimentum eu metus. Aenean eleifend mi ut placerat facilisis. Pellentesque mollis eros eget leo dapibus fermentum sit amet ac nulla. Nullam lacinia odio sed venenatis laoreet. Suspendisse egestas laoreet massa sit amet dapibus. Quisque ac augue mattis, congue turpis vitae, cursus magna. Morbi in turpis libero. Aenean rhoncus nec ante ac lacinia. Praesent ac quam non arcu cursus aliquam in a dui. Etiam vitae arcu dictum, porta urna vel, pellentesque magna. Maecenas in risus diam. Nullam ultricies diam ut velit pellentesque, sed fringilla augue gravida.';
 
+            // descrição
+            p1.innerHTML = 'Projeto desenvolvido durante o curso de desenvolvimento web. Primeiro projeto utilizando HTML, CSS e JavaScript da prática em um projeto. Muito interessante ver como as tecnologias vão se complementando e como o javascript manipula os elementos HTML e o estilo também.';
+
+            // texto para github
+            p2.innerHTML = 'Confira o código no ';
+
+            // link para github
+            gitHub.href = "https://github.com/DouglasFujii/jogo-da-velha"
+            gitHub.innerHTML = 'GitHub';
+            gitHub.target = '_blank';
+
+            // adiciona os elementos html
             modal.appendChild(h1);
-            modal.appendChild(p);
+            modal.appendChild(p1);
+            p2.appendChild(gitHub);
+            modal.appendChild(p2);
 
             break;
-        
+
         case 'churrascometro':
+            // titulo
             h1.innerHTML = 'Churrascometro';
-            p.innerHTML = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec consectetur leo mollis ipsum consequat, in fringilla sem sodales. Phasellus ultricies nec augue suscipit tincidunt. Nunc sagittis nulla at efficitur volutpat. Vestibulum turpis massa, vehicula quis faucibus non, condimentum eu metus. Aenean eleifend mi ut placerat facilisis. Pellentesque mollis eros eget leo dapibus fermentum sit amet ac nulla. Nullam lacinia odio sed venenatis laoreet. Suspendisse egestas laoreet massa sit amet dapibus. Quisque ac augue mattis, congue turpis vitae, cursus magna. Morbi in turpis libero. Aenean rhoncus nec ante ac lacinia. Praesent ac quam non arcu cursus aliquam in a dui. Etiam vitae arcu dictum, porta urna vel, pellentesque magna. Maecenas in risus diam. Nullam ultricies diam ut velit pellentesque, sed fringilla augue gravida.';
 
+            // descrição
+            p1.innerHTML = 'Esse projeto faz parte do curso de desenvolvimento web para fixar conhecimentos básicos em JavaScript adquiridos até o momento. Foi interessante ver a linguagem manipulando os elementos HTML, realizando condicionais e fazendo os cálculos.';
+
+            // texto para github
+            p2.innerHTML = 'Confira o código no ';
+
+            // link para github
+            gitHub.href = 'https://github.com/DouglasFujii/churrascometro';
+            gitHub.innerHTML = 'GitHub';
+            gitHub.target = '_blank';
+
+            // adiciona os elementos html
             modal.appendChild(h1);
-            modal.appendChild(p);
+            modal.appendChild(p1);
+            p2.appendChild(gitHub);
+            modal.appendChild(p2);
 
             break;
 
-        case 'pagina_de_captura':
-            h1.innerHTML = 'Página de Captura';
-            p.innerHTML = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec consectetur leo mollis ipsum consequat, in fringilla sem sodales. Phasellus ultricies nec augue suscipit tincidunt. Nunc sagittis nulla at efficitur volutpat. Vestibulum turpis massa, vehicula quis faucibus non, condimentum eu metus. Aenean eleifend mi ut placerat facilisis. Pellentesque mollis eros eget leo dapibus fermentum sit amet ac nulla. Nullam lacinia odio sed venenatis laoreet. Suspendisse egestas laoreet massa sit amet dapibus. Quisque ac augue mattis, congue turpis vitae, cursus magna. Morbi in turpis libero. Aenean rhoncus nec ante ac lacinia. Praesent ac quam non arcu cursus aliquam in a dui. Etiam vitae arcu dictum, porta urna vel, pellentesque magna. Maecenas in risus diam. Nullam ultricies diam ut velit pellentesque, sed fringilla augue gravida.';
+        case 'site-momo-lamen':
+            // titulo
+            h1.innerHTML = 'Site Momo Lamen';
 
+            // descrição
+            p1.innerHTML = 'Site desenvolvido para um cliente fictício, utilizando HTML e CSS. Esse projeto foi ótimo para fixar conhecimentos e ter a experiência de desenvolver um projeto próprio do zero.';
+
+            // texto para gitHub
+            p2.innerHTML = 'Confira o código no ';
+
+            // link para github
+            gitHub.href = 'https://github.com/DouglasFujii/site-momo-lamen';
+            gitHub.innerHTML = 'GitHub';
+            gitHub.target = '_blank';
+
+            // adiciona os elementos html
             modal.appendChild(h1);
-            modal.appendChild(p);
+            modal.appendChild(p1);
+            p2.appendChild(gitHub);
+            modal.appendChild(p2);
 
             break;
-        
+
         case 'formulario':
+            // titulo
             h1.innerHTML = 'Formulário';
-            p.innerHTML = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec consectetur leo mollis ipsum consequat, in fringilla sem sodales. Phasellus ultricies nec augue suscipit tincidunt. Nunc sagittis nulla at efficitur volutpat. Vestibulum turpis massa, vehicula quis faucibus non, condimentum eu metus. Aenean eleifend mi ut placerat facilisis. Pellentesque mollis eros eget leo dapibus fermentum sit amet ac nulla. Nullam lacinia odio sed venenatis laoreet. Suspendisse egestas laoreet massa sit amet dapibus. Quisque ac augue mattis, congue turpis vitae, cursus magna. Morbi in turpis libero. Aenean rhoncus nec ante ac lacinia. Praesent ac quam non arcu cursus aliquam in a dui. Etiam vitae arcu dictum, porta urna vel, pellentesque magna. Maecenas in risus diam. Nullam ultricies diam ut velit pellentesque, sed fringilla augue gravida.';
 
+            // descrição
+            p1.innerHTML = 'Esse formulário foi desenvolvido para estudar as tags HTML necessárias para se criar um formulário. Esse tipo de demanda é muito comum, então resolvi praticar mais com esse projeto.';
+
+            // texto para gitHub
+            p2.innerHTML = 'Confira o código no ';
+
+            // link para github
+            gitHub.href = 'https://github.com/DouglasFujii/form';
+            gitHub.innerHTML = 'GitHub';
+            gitHub.target = '_blank';
+
+            // adiciona os elementos html
             modal.appendChild(h1);
-            modal.appendChild(p);
+            modal.appendChild(p1);
+            p2.appendChild(gitHub);
+            modal.appendChild(p2);
 
             break;
-        
-        case 'calculadora':
-            h1.innerHTML = 'Calculadora';
-            p.innerHTML = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec consectetur leo mollis ipsum consequat, in fringilla sem sodales. Phasellus ultricies nec augue suscipit tincidunt. Nunc sagittis nulla at efficitur volutpat. Vestibulum turpis massa, vehicula quis faucibus non, condimentum eu metus. Aenean eleifend mi ut placerat facilisis. Pellentesque mollis eros eget leo dapibus fermentum sit amet ac nulla. Nullam lacinia odio sed venenatis laoreet. Suspendisse egestas laoreet massa sit amet dapibus. Quisque ac augue mattis, congue turpis vitae, cursus magna. Morbi in turpis libero. Aenean rhoncus nec ante ac lacinia. Praesent ac quam non arcu cursus aliquam in a dui. Etiam vitae arcu dictum, porta urna vel, pellentesque magna. Maecenas in risus diam. Nullam ultricies diam ut velit pellentesque, sed fringilla augue gravida.';
 
+        case 'calculadora':
+            // titulo
+            h1.innerHTML = 'Calculadora';
+
+            // descrição
+            p1.innerHTML = 'Projeto de uma calculador utilizando JavaScript, visando estudar a linguagem. ';
+
+            // texto para gitHub
+            p2.innerHTML = 'Confira o código no ';
+
+            // link pra github
+            gitHub.href = 'https://github.com/DouglasFujii/calculadora';
+            gitHub.innerHTML = 'GitHub';
+            gitHub.target = '_blank';
+
+            // adiciona os elementos html
             modal.appendChild(h1);
-            modal.appendChild(p);
+            modal.appendChild(p1);
+            p2.appendChild(gitHub);
+            modal.appendChild(p2);
 
             break;
     }
